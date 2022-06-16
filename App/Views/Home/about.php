9a811343ee7497811f0178291373a73c28b7fb80
Karl Lukan <klukan20@gmail.com>
2022-06-15 22:09:08 -0500

    finished about us page

--- App/Views/Home/about.php
+++ App/Views/Home/about.php
@@ -1,14 +1,120 @@
+<div class="bg-textile-dark-blue p-3"></div>
 <div class="container-fluid">
     <div class="row bg-textile-light-gray px-3 py-5">
-        <div class="col-sm-6 text-white mx-auto">
-            <h1 class="h5 mb-5">
+        <div class="col-sm-6 mx-auto">
+            <h1 class="h4 fw-bold mb-5">
                 About LCI Consultants
             </h1>
 
-            <p>
+            <p class="fw-bold">
                 LCI Consultants is a comprehensive consulting firm providing services for existing properties and new construction. 
                 Our experts possess decades of real estate development and construction experience and apply this knowledge to the everyday challenges that owners, contractors, engineers, and architects face maintaining their properties and throughout the construction process of new developments. 
                 LCI provides effective leadership, sharp attention to detail and a valued partnership you can count on throughout the life of your project.
+            </p>
+        </div>
+    </div>
+    <div class="row bg-textile-light-blue py-4">
+        <div class="col-sm-4 mx-auto">
+            <h2 class="h4 text-white mb-0 ms-sm-2">
+                Jay Lukan
+            </h2>
+        </div>
+    </div>
+</div>
+<div class="container mt-3">
+    <div class="row">
+        <aside class="col-sm-3 d-none d-sm-block">
+            <img class="stagger-up border border-4 border-white w-100 mb-3" src="Public/resources/img/about/jay.jpg"/>
+            <a href="https://www.ymcapkc.org/give/" target="_blank">
+                <img class="stagger-up border border-4 border-white w-100" src="Public/resources/img/about/ymca-lci-consultants-banner.jpg"/>
+            </a>
+        </aside>
+        <div class="col-sm-9">
+            <p>
+                Has over twenty five years experience in the management of residential, retail & commercial development and construction.
+                Strong academic background in construction, real estate development, project scheduling, finance and business.
+                Jay has worked on more than 6,000 apartment and condominium units in his career; much of this experience is at a senior level project manager.
+            </p>
+            <p>
+                Jay began his career working for OPUS Southwest Corporation developing and managing the construction of commercial projects.
+                After leaving OPUS he went to work for Trammell Crow Residential in Seattle and then to Intrawest where he was responsible for pre-development, construction cost estimating, and management of hundreds of condominium and apartment homes.
+                He also worked for Intrawest – Mammoth where his role continued to expand on the development side, working with designers, town planners and other stakeholders to develop housing, infrastructure and a golf course in a very eco sensitive area in the Sierra Nevada Mountains.
+            </p>
+            <p>
+                Prior to LCI, Jay was the development director for a Schweitzer Mountain Resort in Idaho.
+                Jay had complete responsibility for all aspects of the development process.
+            </p>
+            <p>
+                Jay co-founded LCI Consultants with his current partners and is very active in the day to day business.
+                Jay’s strengths are in management of the development process from the design phase through final construction completion and closeout.
+                His specific expertise is in; project scheduling, contract negotiations, contractor and design team management, project closeout and plan completion analysis.
+            </p>
+            <p>
+                He also has extensive experience in the cold weather resort development business with construction of golf courses, trail systems, ski lifts, condo-hotels and land development.
+            </p>
+            <p>
+                Jay’s substantial expertise in wood-framed construction, most in the northwest and other challenging climatic regions, make him an excellent resource for identifying building system defects and failure with a comprehensive and cost-effective approach to repairing building problems.
+            </p>
+            <p>
+                Jay is a strong communicator and believes that keeping clients and project teams well informed is one of the keys to a successful project.
+            </p>
+            <p>
+                <b>EDUCATION</b>
+                <br/>
+                BS Construction Engineering Arizona State University 1983 Tempe, AZ
+                MBA Masters in Business Administration Southern Methodist University 1987 Dallas, TX
+            </p>
+            <p>
+                <b>CONTACT</b>
+                <br/>
+                <a class="me-1" href="mailto:jay@lci-consultants.com">
+                    jay@lci-consultants.com
+                </a>
+                |
+                <a href="tel:3606248190">
+                    360-624-8190
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row bg-textile-light-blue py-4">
+        <div class="col-sm-4 mx-auto">
+            <h2 class="h4 text-white mb-0 ms-sm-2">
+                Lisa Lukan
+            </h2>
+        </div>
+    </div>
+</div>
+<div class="container mt-3 mb-5">
+    <div class="row">
+        <aside class="col-sm-3 d-none d-sm-block">
+            <img class="stagger-up border border-4 border-white w-100" src="Public/resources/img/about/lisa.jpg"/>
+        </aside>
+        <div class="col-sm-9">
+            <p>
+                Lisa Lukan has over twenty years experience of real estate development, financial analysis and property management.
+                Lisa began her career as a CPA with an international accounting firm.
+                She developed a passion for real estate development and has focused the last 17 years of her career solely on residential real estate.
+                Because of Lisa’s CPA background, she has all the necessary skills for project financial analysis, budget preparation, management and many legal aspects of a project.
+                Lisa has managed the development, construction, marketing and sales of over a thousand condominium units.
+            </p>
+            <p>
+                <b>EDUCATION</b>
+                <br/>
+                BA Business Administration, University of Washington, 1989
+            </p>
+            <p>
+                <b>CONTACT</b>
+                <br/>
+                <a class="me-1" href="mailto:lisa@lci-consultants.com">
+                    lisa@lci-consultants.com
+                </a>
+                |
+                <a href="tel:3606248189">
+                    360-624-8189
+                </a>
             </p>
         </div>
     </div>
